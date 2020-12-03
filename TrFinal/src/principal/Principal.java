4fb36a9352f4918901e4a070287540d17837716e
yuriAlves5 <yuriAlves5@users.noreply.github.com>
2020-12-03 16:29:48 -0300

    Modificação do menu
    Mudança no formato do menu

--- TrFinal/src/principal/Principal.java
+++ TrFinal/src/principal/Principal.java
@@ -8,9 +8,8 @@ import servicos.Menu;
 public class Principal {
 
 	public static void main(String[] args) {
-		int op;
-		op = Menu.printMenu();
-		Menu.menu(op);
+		
+		Menu.menu();
 		
 		
 
--- TrFinal/src/servicos/Menu.java
+++ TrFinal/src/servicos/Menu.java
@@ -5,41 +5,54 @@ import cadastro.CadastroAluno;
 
 public class Menu {
 	public static int printMenu() {
-		
+
 		String STRopc = null;
-		STRopc = JOptionPane.showInputDialog("Digite 1 para cadastrar um aluno");
+		STRopc = JOptionPane.showInputDialog("Digite 1 para cadastrar um aluno \n"
+											+"Digite 2 para printar os Alunos salvos \n"
+											+"Digite 9 para sair do programa");
 		int opc = Integer.parseInt(STRopc);
-		
+
 		return opc;
 	}
 
-	public static void menu(int i) {
+	public static void menu() {
 		CadastroAluno ca = new CadastroAluno();
 		String STRtemp = null;
-		
 
 		int temp = 0;
+		int menu = 0;
 		do {
+			int i = printMenu();
 			switch (i) {
-			case 1:
-				JOptionPane.showMessageDialog(null, "Cadastrando Aluno");
-
-				if (ca.cadastrarAluno() == true)
-					JOptionPane.showMessageDialog(null, "Aluno cadastrado");
-				else 
-					JOptionPane.showMessageDialog(null, "ERRO");
-				
-				STRtemp = JOptionPane.showInputDialog("Deseja cadastrar outro usuario? 1 = sim 2 = não");
-				temp = Integer.parseInt(STRtemp);
-					break;
 			
+			case 1:
+				do {
+					JOptionPane.showMessageDialog(null, "Cadastrando Aluno");
+
+					if (ca.cadastrarAluno() == true)
+						JOptionPane.showMessageDialog(null, "Aluno cadastrado");
+					else
+						JOptionPane.showMessageDialog(null, "ERRO");
+
+					STRtemp = JOptionPane.showInputDialog("Deseja cadastrar outro usuario? 1 = sim 2 = não");
+					temp = Integer.parseInt(STRtemp);
+					
+				} while (temp != 2);
+
+				break;
+			case 2:
+				ca.printLista();
+				break;
+				
+			case 9:
+				menu = 9;
 
 			default:
 				break;
 			}
-		} while (temp  != 2);
+		} while (menu != 9);
+
 		
-		ca.printLista();
 	}
 
 }
