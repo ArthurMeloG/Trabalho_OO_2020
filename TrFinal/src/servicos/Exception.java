e2c2f87e9fe5084c4084e04333c05a7d4d6e7409
yuriAlves5 <yuriAlves5@users.noreply.github.com>
2020-12-03 21:26:30 -0300

    Adicionando comentario para melhor entimento
    comentario para entendimento da logica da aplicação

--- TrFinal/src/servicos/Exception.java
+++ TrFinal/src/servicos/Exception.java
@@ -12,6 +12,7 @@ public class Exception {
 		int nomeLoop = 0;
 		int emailLoop = 0;
 		int rendLoop = 0;	
+		
 		// NOME EXCEPTION
 
 
@@ -19,17 +20,19 @@ public class Exception {
 			try {
 				
 				String nome = (String) JOptionPane.showInputDialog(null,"Digite o seu nome", "CADASTRO", 0 , Icons.icones(2), null, "");
-				if (nome.isEmpty()) {
+				
+				
+				if (nome.isEmpty()) {  // TESTA O EXCPETION VAZIO
 					throw new RuntimeException();
 				} else
 					nomeLoop = 1;
-					al.setNome(nome);
+					al.setNome(nome);  // CADASTRA NOME AO OBJETO 'al'
 			} catch (RuntimeException exc) {
 				JOptionPane.showMessageDialog(null, "(DadosPessoaisIncompletosException)", "EXCEPTION", JOptionPane.ERROR_MESSAGE);
 				JOptionPane.showMessageDialog(null, "Recadastrando...");
 			}
 
-		} while (nomeLoop != 1);
+		} while (nomeLoop != 1); //LOOP EM CASO DE CADASTRO ERRADO
 		
 		
 		//EMAIL EXCEPTION
@@ -38,17 +41,17 @@ public class Exception {
 			try {
 			
 				String email = (String) JOptionPane.showInputDialog(null,"Digite o seu email","CADASTRO", 0 , Icons.icones(3), null,"");
-				if (email.isEmpty()) {
-					throw new RuntimeException();
+				if (email.isEmpty()) { // TESTA O EXCPETION VAZIO
+					throw new RuntimeException();  // JOGA A EXCEÇÃO
 				} else
 					emailLoop = 1;
-					al.setEmail(email);
+					al.setEmail(email); // CADASTRA EMAIL AO OBJETO 'email'
 			} catch (RuntimeException exc) {
 				JOptionPane.showMessageDialog(null, "(DadosPessoaisIncompletosException)","EXCEPTION", JOptionPane.ERROR_MESSAGE);
 				JOptionPane.showMessageDialog(null, "Recadastrando...");
 			}
 
-		} while (emailLoop != 1);
+		} while (emailLoop != 1);  // LOOP EM CASO DE CADASTRO ERRADO
 		
 
 		// RENDIMENTO EXCEPTION
@@ -57,21 +60,21 @@ public class Exception {
 			try {
 				String strREND = (String) JOptionPane.showInputDialog(null, "Digite o seu rendimento", "CADASTRO", 0 , Icons.icones(4), null, "");
 				float rend = Float.parseFloat(strREND);				
-				al.setRend(rend);
+				al.setRend(rend); //SETA RENDIMENTO AO OBEJTO 'al'
 				rendLoop = 1;
 
-			} catch (NumberFormatException ex) {
+			} catch (NumberFormatException ex) { //TRATA A EXCESSÃO
 
 				JOptionPane.showMessageDialog(null, "(RendimentoInvalidoException)", "EXCEPTION",JOptionPane.ERROR_MESSAGE);
 				JOptionPane.showMessageDialog(null, "Recadastrando...");				
 				rendLoop = 0;
 			}
 
-		} while (rendLoop != 1);
+		} while (rendLoop != 1); // LOOP EM CASO DE CADASTRO ERRADO
 		
 			
 
-		return al;
+		return al; // RETORNA O OBJETO 'al' Completamente preenchido
 
 	}
 
